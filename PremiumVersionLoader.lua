67b721b1cbba452bb626dd035fc1bdb3039cd917
Central <160373638+MufabaScripts@users.noreply.github.com>
2024-06-20 09:41:44 -0700

    Create PremiumVersionLoader.lua

---
+++ PremiumVersionLoader.lua
@@ -0,0 +1,12 @@
+local Place = game.PlaceId
+
+if Place == 7346416636 then
+    warn("Pop It Trading ✨")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MufabaScripts/Peroxy/main/PopItTrading/PremiumVersion.lua"))()
+elseif Place == 7903991471 then
+    warn("Pet Trading")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MufabaScripts/Peroxy/main/PetTrading/PremiumVersion.lua"))()
+elseif Place == 15705682243 then
+    warn("[🧸TOY] Tapping Legends Final")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MufabaScripts/Peroxy/main/TappingFinal/Script.lua"))()
+end
